aa6a14f43eb36ee7c8ca407898e4305ed1d0c536
Arman Alizadeh <armanalizadeh467@gmail.com>
2020-03-18 19:42:07 +0100

    Updates

---
+++ precache-manifest.39678b706414791d9fbd691d0abecfa5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1351fd79ede00db96b915ac8debd366f",
+    "url": "/zoo/index.html"
+  },
+  {
+    "revision": "af7cea5640199dfe8d85",
+    "url": "/zoo/static/css/main.eb73bef4.chunk.css"
+  },
+  {
+    "revision": "28a7d35d5cea2662d2a1",
+    "url": "/zoo/static/js/2.68ad93a4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/zoo/static/js/2.68ad93a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af7cea5640199dfe8d85",
+    "url": "/zoo/static/js/main.88ed95c7.chunk.js"
+  },
+  {
+    "revision": "1afcd1bbb5654641e65b",
+    "url": "/zoo/static/js/runtime-main.ebcb10e8.js"
+  },
+  {
+    "revision": "f9bbd0e11613d32df33318da95fe4590",
+    "url": "/zoo/static/media/bear.f9bbd0e1.png"
+  },
+  {
+    "revision": "dc0ae7e8aab7f99fb4abd4c7adfa1587",
+    "url": "/zoo/static/media/elephant.dc0ae7e8.png"
+  },
+  {
+    "revision": "56578b8e0e1c861f9049f20fbd039018",
+    "url": "/zoo/static/media/giraffe.56578b8e.png"
+  },
+  {
+    "revision": "cf50e7c09f074b30c7679a84f861f697",
+    "url": "/zoo/static/media/hippo.cf50e7c0.png"
+  },
+  {
+    "revision": "188a1b4ffa9b38ffb38344217d0f2d61",
+    "url": "/zoo/static/media/lion.188a1b4f.png"
+  },
+  {
+    "revision": "53b3b055cd2fc33d6091dcd15ef26618",
+    "url": "/zoo/static/media/owl.53b3b055.png"
+  },
+  {
+    "revision": "a6c5a911a3ac3381812a561f0702b256",
+    "url": "/zoo/static/media/rabbit.a6c5a911.png"
+  },
+  {
+    "revision": "4a72bd63b1e77bd80c333d399d20ba72",
+    "url": "/zoo/static/media/sprite.4a72bd63.svg"
+  },
+  {
+    "revision": "6c4a2c5f86fd5fa08f322b5c4bd3f435",
+    "url": "/zoo/static/media/tiger.6c4a2c5f.png"
+  }
+]);
